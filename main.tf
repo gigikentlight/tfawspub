616059fc21077ef3fcca66c1ad0c2bbe084f7567
gigikentlight <gigikentlight@gmail.com>
2020-01-06 11:36:04 -0500

    adf

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+	region 		= "eu-central-1"
+}
+
+resource "aws_instance" "example" {
+	ami 		= "ami-0d4c3eabb9e72650a"
+	instance_type	= "t2.nano"
+
+	tags = {
+		Name 	= "terraform-example"
+	}
+}
+
